fc2c5bd75d9702d6c3a3a7012dba099e1d54fcf9
Maksym Martynenko <predatar24@gmail.com>
2023-03-14 21:29:28 +0200

    Made an adaptive to the 'Product card' page

--- dist/js/script.product-card.js
+++ dist/js/script.product-card.js
@@ -5,4 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
             prevEl: '.card__btn-prev'
         }
     });
-})
+
+    const infoSelect = document.querySelectorAll('.info__list-item');
+    const infoTabs = document.querySelectorAll('.info__tab');
+
+    const toggleActiveClassInformationSelect = (selector, element, toggleClass) => {
+        selector.forEach(elem => {
+            elem.classList.contains(toggleClass) ? elem.classList.remove(toggleClass) : null;
+        });
+        element.classList.add(toggleClass);
+    };
+
+    const toggleActiveClassInformationTabs = (selector, index, toggleClass) => {
+        selector.forEach(elem => {
+            elem.classList.contains(toggleClass) ? elem.classList.remove(toggleClass) : null;
+        });
+        selector[index].classList.add(toggleClass);
+    };
+
+    infoSelect.forEach((elem, index) => {
+        elem.addEventListener('click', () => {
+            toggleActiveClassInformationSelect(infoSelect, elem, 'info__list-item_active');
+            toggleActiveClassInformationTabs(infoTabs, index, 'info__tab_active');
+        });
+    });
+});
