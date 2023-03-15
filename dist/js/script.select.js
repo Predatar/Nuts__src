6f557814403c235d3d694972247af6af49dafb1f
Maksym Martynenko <predatar24@gmail.com>
2023-03-15 23:33:01 +0200

    Added Js to the tab created

--- dist/js/script.select.js
+++ dist/js/script.select.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputSelectCountry = document.querySelector(' #country');
     const inputSelectRegion = document.querySelector('#region');
     const inputSelectCity = document.querySelector('#city');
+    const inputSelectCountryFop = document.querySelector(' #fopCountry');
+    const inputSelectRegionFop = document.querySelector('#fopRegion');
+    const inputSelectCityFop = document.querySelector('#fopCity');
     const selectHead = document.querySelectorAll('.select__head');
+    const selectHeadFop = document.querySelectorAll('.select__fop-head');
     const selectList = document.querySelector('.registration__address .select__list');
+    const selectListFop = document.querySelector('.registration__address .select__fop-list');
     const fopCheck = document.querySelector('#fop');
     const fopInputs = [
         document.querySelector('.registration__props'),
@@ -16,12 +21,22 @@ document.addEventListener('DOMContentLoaded', () => {
         selectRegionMas,
         selectCity,
         selectCityMas;
+    let selectListMasFop = document.querySelectorAll('.registration__address .select__fop-list .select__fop-item'),
+        selectRegionFop,
+        selectRegionMasFop,
+        selectCityFop,
+        selectCityMasFop;
 
     selectListMas = [selectListMas[0], selectListMas[1], selectListMas[2]];
-
-    console.log(fopInputs);
+    selectListMasFop = [selectListMasFop[0], selectListMasFop[1], selectListMasFop[2]];
 
     const address = {
+        country: '',
+        region: '',
+        city: '',
+        address: ''
+    };
+    const addressFop = {
         country: '',
         region: '',
         city: '',
@@ -94,12 +109,65 @@ document.addEventListener('DOMContentLoaded', () => {
     fopCheck.addEventListener('change', () => {
         if (fopCheck.checked) {
             fopInputs.forEach(elem => {
-                elem.removeAttribute('disabled')
-            })
+                elem.removeAttribute('disabled');
+            });
         } else {
             fopInputs.forEach(elem => {
-                elem.setAttribute('disabled', 'disabled')
-            })
+                elem.setAttribute('disabled', 'disabled');
+            });
         }
+    });
+
+    selectHeadFop[0].addEventListener('click', () => {
+        selectListFop.classList.toggle('select__fop-list_show');
+    });
+
+    selectListFop.addEventListener('click', ({ target }) => {
+        addressFop.country = target.getAttribute('data-value');
+        inputSelectCountryFop.value = addressFop.country;
+        selectHeadFop[0].innerHTML = target.innerHTML;
+        selectListMasFop.forEach(elem => {
+            elem.classList.contains('select__fop-item_active')
+                ? elem.classList.remove('select__fop-item_active')
+                : null;
+        });
+        target.classList.add('select__fop-item_active');
+        selectListFop.classList.toggle('select__fop-list_show');
+        selectRegionFop = document.querySelector(`.select__fop-list_${addressFop.country}`);
+        selectRegionMasFop = document.querySelectorAll(
+            `.select__fop-list_${addressFop.country} .select__item`
+        );
+
+        selectRegionFop.addEventListener('click', ({ target }) => {
+            addressFop.region = target.getAttribute('data-value');
+            inputSelectRegionFop.value = addressFop.region;
+            selectHeadFop[1].innerHTML = target.innerHTML;
+            selectRegionMasFop.forEach(elem => {
+                elem.classList.contains('select__fop-item_active')
+                    ? elem.classList.remove('select__fop-item_active')
+                    : null;
+            });
+            target.classList.add('select__fop-item_active');
+            selectRegionFop.classList.toggle('select__fop-list_show');
+            selectCityFop = document.querySelector(`.select__fop-list_${region[`${addressFop.region}`]}`);
+            selectCityMasFop = document.querySelectorAll(
+                `.select__fop-list_${region[`${addressFop.region}`]} .select__fop-item`
+            );
+
+            selectCityFop.addEventListener('click', () => {
+                addressFop.city = selectCityFop.getAttribute('data-value');
+                inputSelectCityFop.value = addressFop.city;
+                selectHeadFop[2].innerHTML = target.innerHTML;
+                selectCityMasFop[0].classList.toggle('select__fop-item_active');
+                selectCityFop.classList.toggle('select__fop-list_show');
+            });
+        });
+    });
+
+    selectHeadFop[1].addEventListener('click', () => {
+        selectRegionFop.classList.toggle('select__fop-list_show');
+    });
+    selectHeadFop[2].addEventListener('click', () => {
+        selectCityFop.classList.toggle('select__fop-list_show');
     });
 });
