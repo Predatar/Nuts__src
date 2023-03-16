624255de2cdb8e26526303bb21f489f7754553e5
Maksym Martynenko <predatar24@gmail.com>
2023-03-16 21:34:21 +0200

    Added js to the 'Password recovery' page

--- dist/js/script.recover-password.js
+++ dist/js/script.recover-password.js
@@ -7,7 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainForm = document.querySelector('.main__form');
     const submainForm = document.querySelector('.submain__form');
 
+    let person, isConfirm;
+
     mainForm.addEventListener('submit', e => {
         e.preventDefault();
-    })
-})
+
+        person = JSON.parse(localStorage.getItem(email.value));
+
+        if (person) {
+            isConfirm = confirm('Вы хотите востановить пароль?');
+        } else {
+            alert('Такой аккаунт не зарегестрирован');
+        }
+    });
+    submainForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (isConfirm) {
+            if (password.value == confirmPassword.value) {
+                person.password = password.value;
+                localStorage.setItem(person.email, JSON.stringify(person));
+            } else {
+                alert('Не совпадают пароли');
+            }
+        } else {
+            alert('Подтвердите или отправьте запрос на подтверждение изменение пароля!');
+        }
+    });
+});
