fb2af8d364f37af8a8f3f18dbfcc7c6bab4cca41
Maksym Martynenko <predatar24@gmail.com>
2023-03-24 09:06:44 +0200

    Added js to tab 'Address details'

--- src/js/script.address.js
+++ src/js/script.address.js
@@ -1,18 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const inputSelectCountry = document.querySelector('#countryFop');
-    const inputSelectRegion = document.querySelector('#regionFop');
+    const inputSelectCountryFop = document.querySelector('#countryFop');
+    const inputselectRegionFop = document.querySelector('#regionFop');
 
-    const selectHead = document.querySelectorAll('.select__head');
+    const inputSelectCountryCorp = document.querySelector(' #countryCorp');
+    const inputSelectRegionCorp = document.querySelector('#regionCorp');
 
-    const selectList = document.querySelector('.address__country .select__list');
+    const inputSelectCountryLegal = document.querySelector(' #countryLegal');
+    const inputSelectRegionLegal = document.querySelector('#regionLegal');
 
-    let selectListMas = document.querySelectorAll('.address__country .select__list .select__item'),
-        selectRegion,
-        selectRegionMas;
+    const selectHeadFop = document.querySelectorAll('.select__head');
+    const selectHeadCorp = document.querySelectorAll('.select__head-corp');
+    const selectHeadLegal = document.querySelectorAll('.select__head-legal');
 
-    selectListMas = [selectListMas[0], selectListMas[1], selectListMas[2]];
+    const selectListFop = document.querySelector('.address__country .select__list');
+    const selectListCorp = document.querySelector('.select__list-corp');
+    const selectListLegal = document.querySelector('.select__list-legal');
+
+    let selectListFopMas = document.querySelectorAll(
+            '.address__country .select__list .select__item'
+        ),
+        selectRegionFop,
+        selectRegionFopMas;
+
+    let selectListMasCorp = document.querySelectorAll('.select__list-corp .select__item'),
+        selectRegionCorp,
+        selectRegionMasCorp;
+
+    let selectListMasLegal = document.querySelectorAll('.select__list-legal .select__item'),
+        selectRegionLegal,
+        selectRegionMasLegal;
+
+    selectListFopMas = [selectListFopMas[0], selectListFopMas[1], selectListFopMas[2]];
+    selectListMasCorp = [selectListMasCorp[0], selectListMasCorp[1], selectListMasCorp[2]];
+    selectListMasLegal = [selectListMasLegal[0], selectListMasLegal[1], selectListMasLegal[2]];
 
     const address = {
+        country: '',
+        region: '',
+        city: ''
+    };
+    const addressCorp = {
+        country: '',
+        region: '',
+        city: ''
+    };
+    const addressLegal = {
         country: '',
         region: '',
         city: ''
@@ -29,41 +61,131 @@ document.addEventListener('DOMContentLoaded', () => {
         'Minsk region': 'mnk'
     };
 
-    selectHead[0].addEventListener('click', () => {
-        selectList.classList.toggle('select__list_show');
+    selectHeadFop[0].addEventListener('click', () => {
+        selectListFop.classList.toggle('select__list_show');
     });
 
-    selectList.addEventListener('click', ({ target }) => {
+    selectListFop.addEventListener('click', ({ target }) => {
         address.country = target.getAttribute('data-value');
-        inputSelectCountry.value = address.country;
-        selectHead[0].innerHTML = target.innerHTML;
-        selectListMas.forEach(elem => {
+        inputSelectCountryFop.value = address.country;
+        selectHeadFop[0].innerHTML = target.innerHTML;
+        selectListFopMas.forEach(elem => {
             elem.classList.contains('select__item_active')
                 ? elem.classList.remove('select__item_active')
                 : null;
         });
         target.classList.add('select__item_active');
-        selectList.classList.toggle('select__list_show');
-        selectRegion = document.querySelector(`.select__list_${address.country}`);
-        selectRegionMas = document.querySelectorAll(
+        selectListFop.classList.toggle('select__list_show');
+        selectRegionFop = document.querySelector(`.select__list_${address.country}`);
+        selectRegionFopMas = document.querySelectorAll(
             `.select__list_${address.country} .select__item`
         );
 
-        selectRegion.addEventListener('click', ({ target }) => {
+        selectRegionFop.addEventListener('click', ({ target }) => {
             address.region = target.getAttribute('data-value');
-            inputSelectRegion.value = address.region;
-            selectHead[1].innerHTML = target.innerHTML;
-            selectRegionMas.forEach(elem => {
+            inputselectRegionFop.value = address.region;
+            selectHeadFop[1].innerHTML = target.innerHTML;
+            selectRegionFopMas.forEach(elem => {
                 elem.classList.contains('select__item_active')
                     ? elem.classList.remove('select__item_active')
                     : null;
             });
             target.classList.add('select__item_active');
-            selectRegion.classList.toggle('select__list_show');
+            selectRegionFop.classList.toggle('select__list_show');
         });
     });
 
-    selectHead[1].addEventListener('click', () => {
-        selectRegion.classList.toggle('select__list_show');
+    selectHeadFop[1].addEventListener('click', () => {
+        selectRegionFop.classList.toggle('select__list_show');
     });
+
+    selectHeadCorp[0].addEventListener('click', () => {
+        selectListCorp.classList.toggle('select__list_show');
+    });
+
+    selectListCorp.addEventListener('click', ({ target }) => {
+        addressCorp.country = target.getAttribute('data-value');
+        inputSelectCountryCorp.value = addressCorp.country;
+        selectHeadCorp[0].innerHTML = target.innerHTML;
+        selectListMasCorp.forEach(elem => {
+            elem.classList.contains('select__item_active')
+                ? elem.classList.remove('select__item_active')
+                : null;
+        });
+        target.classList.add('select__item_active');
+        selectListCorp.classList.toggle('select__list_show');
+        selectRegionCorp = document.querySelector(
+            `.select__head-corp ~ .select__list_${addressCorp.country}`
+        );
+        selectRegionMasCorp = document.querySelectorAll(
+            `.select__list_${addressCorp.country} .select__item`
+        );
+
+        selectRegionCorp.addEventListener('click', ({ target }) => {
+            addressCorp.region = target.getAttribute('data-value');
+            inputSelectRegionCorp.value = addressCorp.region;
+            selectHeadCorp[1].innerHTML = target.innerHTML;
+            selectRegionMasCorp.forEach(elem => {
+                elem.classList.contains('select__item_active')
+                    ? elem.classList.remove('select__item_active')
+                    : null;
+            });
+            target.classList.add('select__item_active');
+            selectRegionCorp.classList.toggle('select__list_show');
+        });
+    });
+
+    selectHeadCorp[1].addEventListener('click', () => {
+        selectRegionCorp.classList.toggle('select__list_show');
+    });
+
+    selectHeadLegal[0].addEventListener('click', () => {
+        selectListLegal.classList.toggle('select__list_show');
+    });
+
+    selectListLegal.addEventListener('click', ({ target }) => {
+        addressLegal.country = target.getAttribute('data-value');
+        inputSelectCountryLegal.value = addressLegal.country;
+        console.log(inputSelectCountryLegal.value);
+        selectHeadLegal[0].innerHTML = target.innerHTML;
+        selectListMasLegal.forEach(elem => {
+            elem.classList.contains('select__item_active')
+                ? elem.classList.remove('select__item_active')
+                : null;
+        });
+        console.log(addressLegal);
+        target.classList.add('select__item_active');
+        selectListLegal.classList.toggle('select__list_show');
+        selectRegionLegal = document.querySelector(
+            `.select__head-legal ~ .select__list_${addressLegal.country}`
+        );
+        console.log(selectRegionLegal);
+        selectRegionMasLegal = document.querySelectorAll(
+            `.select__list_${addressLegal.country} .select__item`
+        );
+
+        selectRegionLegal.addEventListener('click', ({ target }) => {
+            addressLegal.region = target.getAttribute('data-value');
+            inputSelectRegionLegal.value = addressLegal.region;
+            selectHeadLegal[1].innerHTML = target.innerHTML;
+            selectRegionMasLegal.forEach(elem => {
+                elem.classList.contains('select__item_active')
+                    ? elem.classList.remove('select__item_active')
+                    : null;
+            });
+            target.classList.add('select__item_active');
+            selectRegionLegal.classList.toggle('select__list_show');
+        });
+    });
+
+    selectHeadLegal[1].addEventListener('click', () => {
+        selectRegionLegal.classList.toggle('select__list_show');
+    });
+
+    if(localStorage.getItem('corp') == 'true') {
+        document.querySelector('.address__fop').classList.add('address_hide')
+    }
+    else {
+        document.querySelector('.address__corp').classList.add('address_hide')
+    }
 });
